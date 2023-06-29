3723da6c5c6ace0f6dc1965b28b49afb03a3d84b
everton <evertonse.junior@gmail.com>
2023-06-29 03:39:30 -0300

    ball

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Setup
+## Install
+- `git clone https://github.com/evertonse/nvchad-custom.git && cd nvchad-custom && python3 setup.py install`
+## Uninstall 
+- `python3 setup.py install`
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+import sys
+import os
+from pathlib import Path
+from shutil import copytree as copydir, rmtree as rmdir
+
+
+if os.name == 'nt':
+    nvim_path = Path(os.environ['appdata'], 'nvim')
+    nvim_data_path = Path(os.environ['appdata'], 'nvim-data')
+else:
+    nvim_path = Path(f'{Path.home()}', '.config/nvim')
+    nvim_data_path = Path(f'{Path.home()}', '/.local/share/nvim')
+
+
+def install():
+    if nvim_path.exists():
+        if input(f' [setup] {nvim_path.absolute()} already exists do you wanna delete it ? [y/n] ') == 'y':
+            rmdir(nvim_path)
+        else:
+            exit(0)
+    cmd = f'git clone https://github.com/NvChad/NvChad {nvim_path} --depth 1'
+    os.system(cmd)
+
+    try :
+        copydir(Path('./'), Path(nvim_path, 'lua/custom' ) )
+    except Exception as e:
+        print(f'An exception occurred {e}')
+
+
+def unistall():
+    try:
+        print(f'removing {nvim_path}')
+        rmdir(nvim_path)
+        print(f'removing {nvim_path}')
+        rmdir(nvim_data_path)
+    except Exception as e:
+        print(f'An exception occurred {e}')
+
+def usage():
+    print('usage: ./setup.py [install|uninstall]')
+
+def main():
+
+    if len(sys.argv) < 2:
+        usage()
+        exit(1)
+    if sys.argv[1] == 'uninstall':
+        if input('Is you show you wanna uninstall ?[y/n]') == 'y':
+            unistall()
+    elif sys.argv[1] == 'install':
+        install()
+    else:
+        usage()
+main()
+
