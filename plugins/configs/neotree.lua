8c16bdca78385b1f6af15ef1d2fc6a7a24215e9f
evertonse <evertonse.junior@gmail.com>
2023-09-09 22:32:00 -0300

    neotree

--- plugins/configs/neotree.lua
+++ plugins/configs/neotree.lua
@@ -106,13 +106,13 @@ require("neo-tree").setup {
       nowait = true,
     },
     mappings = {
-      ["bc"] = "order_by_created",
-      ["bd"] = "order_by_diagnostics",
-      ["bg"] = "order_by_git_status",
-      ["bm"] = "order_by_modified",
-      ["bn"] = "order_by_name",
-      ["bs"] = "order_by_size",
-      ["bt"] = "order_by_type",
+      ["oc"] = "none",
+      ["od"] = "none",
+      ["og"] = "none",
+      ["om"] = "none",
+      ["on"] = "none",
+      ["os"] = "none",
+      ["ot"] = "none",
 
       ["<space>"] = {
         "toggle_node",
