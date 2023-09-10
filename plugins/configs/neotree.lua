74ee950f1e2ef8a8b352a01b742c27b137f1aabc
evertonse <evertonse.junior@gmail.com>
2023-09-09 22:29:48 -0300

    Sat Sep  9 10:29:48 PM -03 2023

--- plugins/configs/neotree.lua
+++ plugins/configs/neotree.lua
@@ -106,6 +106,14 @@ require("neo-tree").setup {
       nowait = true,
     },
     mappings = {
+      ["bc"] = "order_by_created",
+      ["bd"] = "order_by_diagnostics",
+      ["bg"] = "order_by_git_status",
+      ["bm"] = "order_by_modified",
+      ["bn"] = "order_by_name",
+      ["bs"] = "order_by_size",
+      ["bt"] = "order_by_type",
+
       ["<space>"] = {
         "toggle_node",
         nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
