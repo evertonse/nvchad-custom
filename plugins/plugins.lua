b04d04099f896e24f4d0e884ae8ecb30ca275721
evertonse <evertonse.junior@gmail.com>
2023-10-06 08:56:29 -0300

    Fri Oct  6 08:56:29 AM -03 2023

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -577,7 +577,7 @@ M.plugins = {
     event = "InsertEnter",
     config = function()
       require("better_escape").setup {
-        mapping = { "jk", "jj", "kj" }, -- a table with mappings to use
+        mapping = { "jk", --[[ "jj", ]] "kj" }, -- a table with mappings to use
         timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
         clear_empty_lines = false, -- clear line after escaping if there is only whitespace
         keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
@@ -705,7 +705,7 @@ M.plugins = {
     "lukas-reineke/indent-blankline.nvim",
     -- version = "2.20.7",
     lazy = true,
-    enabled = false,
+    enabled = true,
     init = function()
       require("core.utils").lazy_load "indent-blankline.nvim"
     end,
