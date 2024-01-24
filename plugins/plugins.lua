19472a15188ae323eccbbcf10f386ee0dcdc2ee3
evertonse <evertonse.junior@gmail.com>
2024-01-24 05:26:57 -0300

    Wed Jan 24 05:26:57 AM -03 2024

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -586,6 +586,8 @@ M.plugins = {
         mapping = {
           "jk", --[[ "jj", ]]
           "kj",
+          "jj",
+          "kk",
         }, -- a table with mappings to use
         timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
         clear_empty_lines = true, -- clear line after escaping if there is only whitespace
